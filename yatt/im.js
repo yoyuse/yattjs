57dcc101380575027259282c07e3de6877f0a18d
yoyuse <yoyuse@gmail.com>
2025-10-04 07:50:00 +0900

    不要なコメントを削除

--- yatt/im.js
+++ yatt/im.js
@@ -1,30 +1,3 @@
-// // XXX: CODE COPY
-// // - javascript - Escape regexp strings? - Stack Overflow
-// // - https://stackoverflow.com/questions/6828637/escape-regexp-strings
-// function preg_quote (str, delimiter) {
-//     // Quote regular expression characters plus an optional character
-//     //
-//     // version: 1107.2516
-//     // discuss at: http://phpjs.org/functions/preg_quote
-//     // +   original by: booeyOH
-//     // +   improved by: Ates Goral (http://magnetiq.com)
-//     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-//     // +   bugfixed by: Onno Marsman
-//     // +   improved by: Brett Zamir (http://brett-zamir.me)
-//     // *     example 1: preg_quote("$40");
-//     // *     returns 1: '\$40'
-//     // *     example 2: preg_quote("*RRRING* Hello?");
-//     // *     returns 2: '\*RRRING\* Hello\?'
-//     // *     example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
-//     // *     returns 3: '\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:'
-//     return (str + '').replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
-// }
-
-// if (RegExp.escape === undefined) {
-//     RegExp.escape = function(str) { return preg_quote(str); }
-// }
-// // /XXX: CODE COPY
-
 function IM(id, title, table, table_alt = {}, prefix = {}) {
     this.id = id;
     this.title = title;
